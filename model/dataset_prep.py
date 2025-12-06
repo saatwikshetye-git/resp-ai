fad5a8be8d11c697cca854494c687af43f018874
saatwikshetye-git <saatwikshetye@gmail.com>
2025-12-06 16:07:55 +0530

    Create dataset_prep.py

---
+++ model/dataset_prep.py
@@ -0,0 +1,23 @@
+"""
+dataset_prep.py
+---------------
+Scaffolding for dataset ingestion and preprocessing.
+Actual implementation will be filled in Step 3.
+
+Functions to be implemented:
+- load_coughvid()
+- load_icbhi()
+- load_audioset_subset()
+- unify_datasets()
+- preprocess_and_save()
+- split_train_val_test()
+
+Main pipeline:
+    - Load all datasets
+    - Unify labels
+    - Preprocess audio
+    - Save into data/processed/train, val, test
+"""
+
+# PLACEHOLDER
+print("dataset_prep scaffold loaded")
